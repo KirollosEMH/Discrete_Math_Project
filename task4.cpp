e36b68e1bebae1e2b94736101651f38890585e82
AHKSASE2002 <21p0153@eng.asu.edu.eg>
2023-12-27 02:04:24 +0200

    update: task 4

--- task4.cpp
+++ task4.cpp
@@ -1,47 +1,50 @@
-#include <iostream>
-#include <string>
+#include <iostream> 
+#include <string> // For using the string type
 
-using namespace std;
+using namespace std; 
 
-int main() {
+// Deciphering a text using the Affine cipher
+int main() { 
     int alphabet[27] = {0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
+                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; 
     int a = 0, b = 0;
 
-    cout << "Type your Deciphering keys: a =? & b =? ";
-    cin >> a >> b;
+    // The keys of the cipher are the integers a and b, where a is chosen to be relatively prime to 27
+    cout << "Type your Deciphering keys: (a =? & b =?)    "; 
+
+    cin >> a >> b; // Read the keys
 
     // Find the multiplicative inverse of 'a' in the group of integers modulo 27
     int a_inverse = 0;
-    for (int i = 0; i < 27; ++i) {
-        if ((a * i) % 27 == 1) {
-            a_inverse = i;
-            break;
+    for (int i = 0; i < 27; ++i) { // For each integer in the group of integers modulo 27
+        if ((a * i) % 27 == 1) { // If the multiplicative inverse of 'a' is found
+
+            a_inverse = i; // Set the multiplicative inverse of 'a'
+
+            break; 
         }
     }
 
     cin.ignore(); // Clear the input buffer
-    cout << "\nEnter the ciphered text: ";
-    string text;
-    getline(cin, text);
+    cout << "\nEnter the ciphered text: "; 
 
-    cout << "\nDeciphered text:\n";
+    // Read the text to be deciphered
+    string text;  
+    getline(cin, text); // Read the whole line
+
+    cout << "\nDeciphered text:\n"; 
 
     int charCount = 0;
-    for (char c : text) {
-        int charIndex = (c == ' ') ? 0 : c - 'A' + 1;
+    // Decipher the text
+    for (char c : text) { // For each character in the text convert the character to an index in the alphabet
+        int charIndex = (c == ' ') ? 0 : c - 'A' + 1;  
 
-        int decoded_index = (a_inverse * (charIndex - b + 27)) % 27;
-        cout << static_cast<char>(alphabet[decoded_index]);
+        int decoded_index = (a_inverse * (charIndex - b + 27)) % 27; // Decipher the character
+        cout << static_cast<char>(alphabet[decoded_index]); // Print the deciphered character
 
-        // Insert a space every 3 characters
-        if (++charCount % 3 == 0) {
-            cout << " ";
-        }
     }
 
-    cout << endl;
+    cout << endl; 
 
-    return 0;
+    return 0; 
 }
